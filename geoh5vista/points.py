e13e91005aa593c5cab1ae3997ab43ae53b664cb
Derek Kinakin <dkinakin@gmail.com>
2024-05-19 19:33:06 -0700

    Initial vtk to points and vtk to surface functions
    Initial functions to convert VTK to points and surface geometries. Adding data not complete yet.

--- geoh5vista/points.py
+++ geoh5vista/points.py
@@ -33,13 +33,17 @@ def points_to_vtk(pts, origin=(0.0, 0.0, 0.0)):
     return output
 
 
-#def vtk_to_points(vtk, workspace, name):
-#    points = Points.create(
-#        workspace=workspace,
-#        vertices=vtk.points,
-#        name=name)
-#    return points
+def vtk_geom_to_points_geom(vtk_obj, gh5wkspc, obj_nm):
+    """Convert a VTK object to a points geometry object
+
+    Args:
+        vtk_obj (:class:`pyvista.PolyData`): the VTK object to convert
+    """
+    points = Points.create(workspace=gh5wkspc,
+                           vertices=vtk_obj.points,
+                           name=obj_nm)
+    return points
     
 
 points_to_vtk.__displayname__ = "Points to VTK"
-#vtk_to_points.__displayname__ = "VTK to Points"
+vtk_geom_to_points_geom.__displayname__ = "VTK to Points Geometry"
--- geoh5vista/surface.py
+++ geoh5vista/surface.py
@@ -9,6 +9,8 @@ __displayname__ = "Surface"
 
 import numpy as np
 import pyvista
+from geoh5py.workspace import Workspace
+from geoh5py.objects import Surface
 
 from geoh5vista.utilities import add_data, add_texture_coordinates
 
@@ -44,7 +46,25 @@ def surface_to_vtk(trisurf, origin=(0.0, 0.0, 0.0)):
     return output
 
 
-surface_to_vtk.__displayname__ = "Surface to VTK"
-surface_geom_to_vtk.__displayname__ = "Surface Geometry to VTK"
+def vtk_geom_to_surface_geom(vtk_obj, gh5wkspc, obj_nm):
+    """Convert a VTK object to a surface geometry object
+
+    Args:
+        vtk_obj (:class:`pyvista.PolyData` or :class:'pyvista.UnstructuredGrid'): the VTK object to convert
+    """
+    vrts = vtk_obj.points
+    if isinstance(vtk_obj, pyvista.PolyData):
+        clls = vtk_obj.faces.reshape((vtk_obj.n_faces, 4))[:, 1:]
+    elif isinstance(vtk_obj, pyvista.UnstructuredGrid):
+        clls = vtk_obj.cells.reshape((vtk_obj.n_cells, 4))[:, 1:]
+    else:
+        raise ValueError("Mesh type not supported.")
+    
+    srfc = Surface.create(gh5wkspc, vertices=vrts, cells=clls, name=obj_nm)
+    return srfc
+
 
 surface_to_vtk.__displayname__ = "Surface to VTK"
+surface_geom_to_vtk.__displayname__ = "Surface Geometry to VTK"
+surface_to_vtk.__displayname__ = "Surface to VTK"
+vtk_geom_to_surface_geom.__displayname__ = "VTK to Surface Geometry"
