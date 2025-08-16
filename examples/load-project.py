dd4d241d9e26a14225ecd31414d96348f8fee6f3
Derek Kinakin <89031238+derek-kinakin@users.noreply.github.com>
2025-08-16 12:33:59 -0700

    update example for data types and colours

--- examples/load-project.py
+++ examples/load-project.py
@@ -5,27 +5,24 @@ Load Project
 Load and visualize an GEOH5 project file
 """
 
-import sys
+
 import pyvista as pv
-from pathlib import Path
 import geoh5vista
+from pathlib import Path
 
 path_root = Path(__file__).parents[1]
-sys.path.append(str(path_root))
-
-
 
 ###############################################################################
 # Load the project into an :class:`pyvista.MultiBlock` dataset
 
-project = geoh5vista.load_project(f"{path_root}\\assets\\test_1_file.geoh5")
+project = geoh5vista.load_project(f"../assets/test_1_file.geoh5")
 print(project)
 
 ###############################################################################
 # Once the data is loaded as a :class:`pyvista.MultiBlock` dataset from
 # ``geoh5vista``, then that object can be directly used for interactive 3D
 # visualization from ``pyvista``:
-
+print(type(project))
 project.plot()
 
 ###############################################################################
@@ -38,19 +35,13 @@ vol = project["Block Model"]
 assay = project["wolfpass_WP_assay"]
 topo = project["Topography"]
 dacite = project["Dacite"]
+diorite = project["Early Diorite"]
 
 ###############################################################################
 
 assay.set_active_scalars("DENSITY")
 
-p = pv.Plotter()
-p.add_mesh(assay.tube(radius=3))
-p.add_mesh(topo, opacity=0.5)
-p.show()
-
-###############################################################################
 # Then apply a filtering tool from ``pyvista`` to the volumetric data:
-
 # Threshold the volumetric data
 thresh_vol = vol.threshold([1.09, 4.20])
 print(thresh_vol)
@@ -61,20 +52,52 @@ print(thresh_vol)
 # Create a plotting window
 p = pv.Plotter()
 # Add the bounds axis
-p.show_bounds()
-p.add_bounding_box()
+# p.show_bounds()
+# p.add_bounding_box()
 
 # Add our datasets
-p.add_mesh(topo, opacity=0.5)
+p.add_mesh(
+    topo,
+    color=topo.user_dict["colour"],
+    opacity=0.5,
+    label=topo.user_dict["name"],
+    )
+
 p.add_mesh(
     dacite,
-    color="orange",
+    color=dacite.user_dict["colour"],
     opacity=0.6,
-)
-p.add_mesh(thresh_vol, cmap="coolwarm", clim=vol.get_data_range())
+    label=dacite.user_dict["name"],
+    )
+
+p.add_mesh(
+    diorite,
+    color=diorite.user_dict["colour"],
+    opacity=0.6,
+    label=diorite.user_dict["name"],
+    )
+
+#p.add_mesh(
+#    thresh_vol,
+#    cmap="coolwarm",
+#    )
+
+p.add_mesh_threshold(
+    thresh_vol,
+    cmap="coolwarm", pointa=(0.05, 0.9), pointb=(0.4, 0.9),
+    )
 
 # Add the assay logs: use a tube filter that varius the radius by an attribute
-p.add_mesh(assay.tube(radius=3), cmap="viridis")
+p.add_mesh(
+    assay.tube(radius=3),
+    cmap="viridis",
+    label="assay",
+    )
 
-#p.export_html("geoh5vista.html")
+# Plotter settings
+p.set_viewup([0, 1, 0])
+p.add_axes()
+p.add_legend(size=(0.1, 0.1))
+#p.enable_parallel_projection()
+#p.export_html("geoh5vista_example.html")
 p.show()
