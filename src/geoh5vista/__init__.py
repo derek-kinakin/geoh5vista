9ee8205ab4cef838096cee830a81e112bb01ecca
Derek Kinakin <89031238+derek-kinakin@users.noreply.github.com>
2025-09-27 14:23:22 -0700

    updates to package level init to clean up public API.

--- src/geoh5vista/__init__.py
+++ src/geoh5vista/__init__.py
@@ -1,21 +1,36 @@
-"""``geoh5vista``: 3D visualization for the Geoh5 format (geoh5)
+"""``geoh5vista``: 3D visualization and geometry processing for Geoh5 format (geoh5) objects.
 """
 
-from geoh5vista.wrapper import read_workspace, entities_to_vtk
+from geoh5vista.blockmodel import blockmodel_to_vtk
+from geoh5vista.curve import curve_to_vtk, vtk_to_curve
+from geoh5vista.drillholes import drillholes_to_vtk
+from geoh5vista.grid2d import grid2d_to_vtk
+from geoh5vista.octree import octree_to_vtk
+from geoh5vista.points import points_to_vtk, vtk_to_points
+from geoh5vista.surface import surface_to_vtk, vtk_to_surface
+from geoh5vista.wrapper import geoh5wrap, read_workspace
 
 # Package meta data
 __author__ = "Derek Kinakin"
 __license__ = "BSD-3-Clause"
 __copyright__ = "2024, Derek Kinakin"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __displayname__ = "GEOH5-VTK"
 __name__ = "geoh5vista"
 
 
-def ignore_warnings():
-    """Sets a warning filter for pillow's annoying ``DecompressionBombWarning``"""
-    import warnings
-    from PIL import Image
-    warnings.simplefilter(action="ignore", category=Image.DecompressionBombWarning)
+__all__ = [
+    "blockmodel_to_vtk",
+    "curve_to_vtk",
+    "drillholes_to_vtk",
+    "grid2d_to_vtk",
+    "octree_to_vtk",
+    "points_to_vtk",
+    "surface_to_vtk",
+    "vtk_to_curve",
+    "vtk_to_points",
+    "vtk_to_surface",
+    "read_workspace",
+    "geoh5wrap",
+]
 
-ignore_warnings()
