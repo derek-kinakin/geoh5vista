f15dde651743cbef85ea593f772ebfafd1af52fa
Derek Kinakin <89031238+derek-kinakin@users.noreply.github.com>
2026-01-10 20:47:31 -0800

    Prototyping code for writing block model objects (ImageData or StructuredGrid) to geoh5.

--- src/geoh5vista/blockmodel.py
+++ src/geoh5vista/blockmodel.py
@@ -4,17 +4,21 @@ __all__ = [
     "get_blockmodel_shape",
     "blockmodel_grid_geom_to_vtk",
     "blockmodel_to_vtk",
+    "vtk_geom_to_blockmodel",
+    "vtk_to_blockmodel"
 ]
 
 __displayname__ = "Blockmodel"
 
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 import numpy as np
 import pyvista
 
+from geoh5py.objects.object_base import ObjectBase
 from geoh5py.objects.block_model import BlockModel
+from geoh5py.workspace.workspace import Workspace
 from geoh5py.shared.utils import xy_rotation_matrix
-from geoh5vista.data import add_data_to_vtk_grid, add_entity_metadata
+from geoh5vista.data import add_data_to_vtk_grid, add_entity_metadata, add_data_to_geoh5
 
 
 def get_blockmodel_shape(bm: BlockModel) -> Tuple[int, int, int]:
@@ -154,7 +158,87 @@ def blockmodel_to_vtk(blkmdl: BlockModel) -> pyvista.DataSet:
     return output
 
 
+def vtk_geom_to_blockmodel(vtk: Union[pyvista.ImageData, pyvista.StructuredGrid], workspace: Workspace, name: str) -> BlockModel:
+    """Convert a ``pyvista.ImageData`` or ``pyvista.StructuredGrid`` object to a ``geoh5py.objects.block_model.BlockModel`` object.
+
+    Parameters
+    ----------
+    vtk : Union[pyvista.ImageData, pyvista.StructuredGrid]
+        The VTK object to convert. It must have the required dimensions for a block model (nU x nV x nZ).
+    workspace : geoh5py.workspace.Workspace
+        The geoh5py workspace to add the new surface to.
+    name : str
+        The name of the new block model.
+
+    Returns
+    -------
+    geoh5py.objects.block_model.BlockModel
+        The newly created block model.
+
+    Raises
+    ------
+    ValueError
+        If the VTK object does not have the required dimensions for a block model (nU x nV x nZ).
+
+    """
+
+    #TO DO: Add checks for required dimensions. Cells must be uniform size in each direction. Cells must be 6 sided.
+    #TO DO: Extract cell sizes and origin correctly
+    #TO DO: Extract rotation correctly
+
+    origin = vtk.origin
+    spacing = vtk.spacing
+    n_blocks_x = vtk.dimensions[0] - 1
+    n_blocks_y = vtk.dimensions[1] - 1
+    n_blocks_z = vtk.dimensions[2] - 1
+    block_x_dim = spacing[0]
+    block_y_dim = spacing[1]
+    block_z_dim = spacing[2]
+    rotation = 0.0  # TO DO: Extract rotation from vtk.direction_matrix if
+
+    blockmodel = BlockModel.create(
+        workspace,
+        origin=origin,
+        u_cell_delimiters=np.cumsum(np.pad(np.ones(n_blocks_x) * block_x_dim, (1, 0), "constant")),  # Offsets along u
+        v_cell_delimiters=np.cumsum(np.pad(np.ones(n_blocks_y) * block_y_dim, (1, 0), "constant")),  # Offsets along v
+        z_cell_delimiters=np.cumsum(np.pad(np.ones(n_blocks_z) * block_z_dim, (1, 0), "constant")),  # Offsets along z
+        rotation=rotation,
+        name=name,
+    )
+    return blockmodel
+
+
+def vtk_to_blockmodel(vtk: Union[pyvista.ImageData, pyvista.StructuredGrid], workspace: Workspace, name: str) -> ObjectBase:
+    """Convert a ``pyvista.ImageData`` or ``pyvista.StructuredGrid`` object to a ``geoh5py.objects.block_model.BlockModel`` object.
+
+    This is a wrapper for ``vtk_geom_to_surface`` and is intended to be the
+    main entry point for this conversion. In the future, it will also handle
+    transferring data from the VTK object to the geoh5py object.
+
+    Parameters
+    ----------
+    vtk : Union[pyvista.ImageData, pyvista.StructuredGrid]
+        The VTK object to convert. It must have the required dimensions for a block model (nU x nV x nZ).
+    workspace : geoh5py.workspace.Workspace
+        The geoh5py workspace to add the new surface to.
+    name : str
+        The name of the new block model.
+
+    Returns
+    -------
+    geoh5py.objects.block_model.BlockModel
+        The newly created block model.
+
+
+    """
+    blockmodel = vtk_geom_to_blockmodel(vtk=vtk, workspace=workspace, name=name)
+    blockmodel = add_data_to_geoh5(blockmodel, vtk) # may need a new function to ensure data is aligned to the grid cells correctly
+    return blockmodel
+
+
 # Now set up the display names for the docs
 blockmodel_to_vtk.__displayname__ = "Blockmodel to VTK" # type: ignore
 blockmodel_grid_geom_to_vtk.__displayname__ = "Blockmodel Grid Geometry to VTK" # type: ignore
 get_blockmodel_shape.__displayname__ = "Blockmodel Shape" # type: ignore
+vtk_geom_to_blockmodel.__displayname__ = "VTK Geometry to Blockmodel" # type: ignore
+vtk_to_blockmodel.__displayname__ = "VTK to Blockmodel" # type: ignore
